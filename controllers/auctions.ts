883820f1e0f40df06d0d36d6e87d3aa38ebce49d
fajarma <maulana.fajar@tokopedia.com>
2020-12-03 13:55:42 +0700

    view all auction & view auction

---
+++ controllers/auctions.ts
@@ -0,0 +1,84 @@
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+
+import { dbCreds } from "../config.ts";
+import mapResponse from "../helpers/mapResponse.ts";
+
+const PER_PAGE = 2;
+
+// Init Client
+const client = new Client(dbCreds);
+
+// @desc    Get all auctions
+// @route   GET /api/v1/common/all-auctions
+
+export const getAllAuctions = async ({ request, response }: { request: any, response: any }) => {
+  try {
+    const requestURL = request.url;
+    const userId = requestURL.searchParams.get('user_id');
+    let page = Number(requestURL.searchParams.get('page'));
+    const userType = requestURL.searchParams.get('type');
+
+    await client.connect();
+
+    let additional = "";
+
+    if (userType === 'seller') {
+      additional = `WHERE user_id = ${userId}`;
+    } else if (userType === 'buyer') {
+      additional = `WHERE status = 3`;
+    }
+
+    if (!page || isNaN(page)) {
+      page = 1;
+    }
+    const paging = `LIMIT ${PER_PAGE} OFFSET ${(page - 1) * PER_PAGE}`;
+
+    const result = await client.query(`SELECT * FROM auctions ${additional} ORDER BY created_date, auction_id ASC ${paging}`);
+    const auctionsTemp = mapResponse(result);
+
+    const dataCount = await client.query(`SELECT COUNT(*) FROM auctions ${additional}`);
+    const count = Number(dataCount.rows[0][0]);
+
+    response.body = {
+      success: true,
+      data: auctionsTemp,
+      hasNext: count - (page * PER_PAGE) > 0,
+    };
+  } catch (e) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      errorMessage: e.message || "No data",
+    };
+  }
+}
+
+// @desc    get a auction
+// @route   GET /api/v1/common/auction?id=:id
+
+export const getAuction = async ({ request, response }: { request: any, response: any }) => {
+  try {
+    const id = request.url.searchParams.get('id');
+    if(!id) {
+      throw new Error("Auction id is required");
+    } else if (isNaN(Number(id))) {
+      throw new Error("Auction id is not valid");
+    }
+
+    await client.connect();
+
+    const result = await client.query(`SELECT * FROM auctions WHERE auction_id = ${id}`);
+    const auctionsTemp = mapResponse(result);
+
+    response.body = {
+      success: true,
+      data: auctionsTemp[0] || null,
+    };
+  } catch (e) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      errorMessage: e.message || "No data",
+    };
+  }
+}
--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
-import { Router } from 'https://deno.land/x/oak/mod.ts'
+import { Router, helpers } from 'https://deno.land/x/oak/mod.ts'
 
 import { getProducts, getProduct, addProduct, updateProduct, deleteProduct } from './controllers/products.ts'
+import { getAllAuctions, getAuction } from './controllers/auctions.ts'
 
 const router = new Router()
 
@@ -9,5 +10,8 @@ router.get('/api/v1/products/:id',getProduct)
 router.post('/api/v1/products',addProduct)
 router.put('/api/v1/products/:id',updateProduct)
 router.delete('/api/v1/products/:id',deleteProduct)
+
+router.get('/api/v1/common/all-auctions', getAllAuctions);
+router.get('/api/v1/common/auction', getAuction);
 
 export default router
