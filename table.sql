007f0e417629e3541adbb7cf738dfa4a35580c5c
Fajar Maulana <44862078+fajarma@users.noreply.github.com>
2020-12-04 16:38:56 +0700

    Fajar/auction notif (#5)
    * update bid api
    
    * update gql

--- table.sql
+++ table.sql
@@ -5,22 +5,24 @@
 -- DROP TABLE public.auctions;
 
 CREATE TABLE public.auctions (
-	auction_id serial NOT NULL,
+	auction_id integer NOT NULL,
 	user_id numeric NOT NULL,
+	shop_name text NOT NULL,
 	product_name text NOT NULL,
 	product_description text NOT NULL,
-	product_image text NOT NULL,
-	multiple int4 NOT NULL,
-	initial_price int4 NOT NULL,
-	final_price int4 NOT NULL,
-	highest_bid int4 NOT NULL,
-	duration int4 NOT NULL,
-	status int4 NOT NULL,
-	start_date date NULL,
-	end_date date NULL,
-	reasons text NULL,
-	created_date date NOT NULL,
-	updated_date date NOT NULL,
+	product_images text[] NOT NULL,
+	multiple integer NOT NULL,
+	initial_price integer NOT NULL,
+	final_price integer NOT NULL,
+	highest_bid integer DEFAULT 0,
+	duration integer NOT NULL,
+	status integer DEFAULT 1,
+	start_date timestamp(0) without time zone,
+	end_date timestamp(0) without time zone,
+	reasons text,
+	created_date timestamp(0) without time zone NOT NULL,
+	updated_date timestamp(0) without time zone NOT NULL,
+	bid_count integer DEFAULT 0,
 	CONSTRAINT auctions_pk PRIMARY KEY (auction_id)
 );
 
@@ -36,7 +38,17 @@ CREATE TABLE public.bidding_history (
 	auction_id serial NOT NULL,
 	user_id numeric NOT NULL,
 	bid int4 NOT NULL,
-	bid_date date NOT NULL,
+	bid_date timestamp(0) NULL,
 	CONSTRAINT bidding_history_pk PRIMARY KEY (bidding_history_id),
 	CONSTRAINT bidding_history_fk FOREIGN KEY (auction_id) REFERENCES auctions(auction_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE public.notification (
+	notification_id serial NOT NULL,
+	title text NOT NULL,
+	detail text NOT NULL,
+	created_date timestamp(0) NULL,
+	user_id int4 NOT NULL,
+	"type" varchar NOT NULL,
+	is_read bool NOT NULL DEFAULT false
 );
